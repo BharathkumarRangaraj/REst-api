f8e696834953e8a324b92968070b5b8310a1cb53
Bharath <rbharathkumar12.com>
2024-04-28 08:43:02 +0530

    fetch and error handling

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,42 @@
 import Places from './Places.jsx';
+import { useState,useEffect } from 'react';
+import Error from '../../Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const[places,setplaces]=useState([]);
+  const[isfetching,setisfetching]=useState();
+  const [error,seterror]=useState()
+  useEffect(()=>{
+    setisfetching(true)
+    
+      async function getApi(){
+        try{
+        const data=await fetch('http://localhost:3000/placesf');
+        const json=await data.json();
+        if(!data.ok){
+          throw new Error('failed to fetch places')
+        }
+        setplaces(json.places);
+    
+    
+      
+    }catch(error){
+      seterror({message:error.message || 'could not found places,try again'})
+    }
+    setisfetching(false);
+      }
+    getApi();
+  },[]);
+  
+  if (error){
+    return <Error title='error occured' message={error.message} />
+  }
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={places}
+      fetching={isfetching}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
